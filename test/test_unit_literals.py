ff5c5533ee70475ec2cb8eabd53c5f26ae77013c
Adam Hupp <adam@hupp.org>
2023-04-28 13:09:04 -0700

    delete stale things, add real tests

--- test/test_unit_literals.py
+++ test/test_unit_literals.py
@@ -1,21 +1,60 @@
-import unittest
+import unit_literals
+import pytest
+import numpy
+import pint
 
-class Test(unittest.TestCase):
-    def test_transforms(self):
-        from IPython.testing.globalipapp import get_ipython
-        ip = get_ipython()
-        ip.run_cell("import unit_literals.autohook")
-        res = ip.run_cell("""
-d = 1 lightyear
-t = 1 fortnight
-speed = d / t
 
-how_far = speed * 1 millisecond
+class AttrTest:
+    def __init__(self):
+        self.second = 7
 
-print(how_far.to_compact())
-print(how_far.to_base_units())
-print(how_far.ito_base_units())
-how_far
-""")
-        print(res)
 
+attr_test = AttrTest()
+
+dict_test = {"value": 37}
+
+second = 1024
+
+
+def dbg_transform(code):
+    from pprint import pprint
+
+    tokens = list(unit_literals.generate_tokens(code))
+    pprint(tokens)
+    pprint(unit_literals.parse(iter(tokens)))
+    pprint(unit_literals.transform(code))
+
+
+def do_mult(left, right):
+    return left * right
+
+
+def assert_quantity(code, value, units):
+    result = eval(unit_literals.transform(code), globals())
+
+    if type(value) == float and type(result.magnitude) == float:
+        assert result.magnitude == pytest.approx(value)
+    elif type(result.magnitude) == numpy.ndarray:
+        assert numpy.all(result.magnitude == value)
+    else:
+        assert result.magnitude == value
+    assert result.units == unit_literals.ureg.Unit(units)
+
+
+def test_all():
+
+    assert_quantity("12 meter", 12, "meter")
+    assert_quantity("13 meter/s**2", 13, "meter/s**2")
+    assert_quantity("2048 meter/second * 2 second", 4096, "meters")
+    assert_quantity("(2048 meter)/second * 2 second", 4, "meter*second")
+    assert_quantity("do_mult(3 kg, 5 s)", 15, "kg*s")
+    assert_quantity("(3 kg) pounds", 6.61386786, "pounds")
+    assert_quantity("[2, 5] T", [2, 5], "T")
+    assert_quantity("attr_test.second degF", 7, "degF")
+    assert_quantity("dict_test['value'] ns", 37, "ns")
+    with pytest.raises(pint.errors.DimensionalityError):
+        assert_quantity("2**4 meters", 16, "meters")
+    assert_quantity("second * 1 meters", 1024, "meters")
+
+    with pytest.raises(SyntaxError):
+        assert_quantity("3 smoots", 3, "smoots")
