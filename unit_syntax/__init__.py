6cce24d8b3fe364bf2ef080143067df1906ad52a
Adam Hupp <adam@hupp.org>
2023-07-12 13:09:44 -0700

    print reduced units

--- unit_syntax/__init__.py
+++ unit_syntax/__init__.py
@@ -3,8 +3,6 @@ import pint
 # Use the _DEFAULT_REGISTRY so we can interoperate with other pint-using libraries
 ureg = pint._DEFAULT_REGISTRY
 
-DEBUG = False
-
 
 def Quantity(value, units):
     if isinstance(value, ureg.Quantity):
@@ -13,7 +11,26 @@ def Quantity(value, units):
         return ureg.Quantity(value, units)
 
 
-def enable_ipython(debug=False):
+def _format_quantity_markdown(q):
+    return q.to_reduced_units()._repr_markdown_()
+
+
+def _format_quantity_html(q):
+    return q.to_reduced_units()._repr_html_()
+
+
+def _format_quantity_pretty(q, p, cycle):
+    return q.to_reduced_units()._repr_pretty_(p, cycle)
+
+
+def _format_quantity_latex(q):
+    return q.to_reduced_units()._repr_latex_()
+
+
+def enable_ipython(debug_transform=False):
+    """
+    debug_transform -- if True, log the input and output of each transform
+    """
     import IPython
     from .transform import transform_lines
 
@@ -21,20 +38,32 @@ def enable_ipython(debug=False):
     if not hasattr(ip, "input_transformers_post"):
         raise ImportError("Unsupported IPython version, version >=7 is required")
 
-    if debug:
+    if debug_transform:
         import logging
 
         logging.basicConfig(level=logging.DEBUG, force=True)
 
         def transform_lines_dbg(lines):
             ret = transform_lines(lines)
-            logging.debug("unit_syntax: %s -> %s", lines, ret)
+            logging.debug("unit_syntax: %s -> %s", "\n".join(lines), "\n".join(ret))
             return ret
 
         do_transform = transform_lines_dbg
     else:
         do_transform = transform_lines
     ip.input_transformers_post.append(do_transform)
+
+    # Overide default formatters to use reduced units.  This lets us keep using the
+    # default registry for interop with other pint-using libraries, but gives more
+    # sensible output in the notebook.
+    def add_formatter(mime, fn):
+        formatter = ip.display_formatter.formatters[mime]
+        formatter.for_type(pint.Quantity, fn)
+
+    add_formatter("text/markdown", _format_quantity_markdown)
+    add_formatter("text/html", _format_quantity_html)
+    add_formatter("text/plain", _format_quantity_pretty)
+    add_formatter("text/latex", _format_quantity_latex)
 
     # ensure the module is still visible if imported via
     # `from unit_syntax import ipython` for some reason
